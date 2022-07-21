8aeba31fe601f84e1354dcf4549660e342aeb202
Ashley Wong <tofudragon333@gmail.com>
2022-07-21 00:55:26 -0400

    commit tree 0df67aecf79b9800d4137f178e3997dd8f230ada

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,9 +18,32 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  #PATCH /plants/:id
+  def update
+    plant=Plant.find_by(id: params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant, status: :ok
+    else
+      render json: {error: "Plant not found"}, status: :not_found
+    end
+  end
+
+  def destroy
+    plant=Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+
+    end
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
 end
